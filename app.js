12a8ab3f69b1f0578b043cdb8785b9204dd9b47e
macy260admin <macy260admin@naokita-2.local>
2017-02-19 17:09:51 -0500

    fixed starting script

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var io = require('socket.io').listen(3000); // initiate socket.io server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' }); // Send data to client
-
-  // wait for the event raised by the client
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
 });
+
+server.listen(4200); 
