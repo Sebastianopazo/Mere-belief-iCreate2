f62a51740f736b074630ca64d7d9dcc9f977ad0e
macy260admin <macy260admin@naokita-2.local>
2017-02-19 17:38:00 -0500

    fixed starting script

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ io.on('connection', function(client) {
     });
   });
 
+  io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+
 server.listen(3000, function(){
   console.log('listening on *:3000');
 });
