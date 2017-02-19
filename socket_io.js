648846db6e76cdc0a9fb8ab07108d335ed42d084
macy260admin <macy260admin@naokita-2.local>
2017-02-19 00:54:55 -0500

    configuring socket.io

--- socket_io.js
+++ socket_io.js
@@ -1,9 +1,24 @@
-var io = require('socket.io').listen(80); // initiate socket.io server
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' }); // Send data to client
+app.listen(80);
 
-  // wait for the event raised by the client
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
