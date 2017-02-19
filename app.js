0a86ec2054c75ae78c192face7f0395029252cd4
macy260admin <macy260admin@naokita-2.local>
2017-02-19 17:03:02 -0500

    fixed starting script

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(3000); // initiate socket.io server
+var io = require('socket.io').listen(80); // initiate socket.io server
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' }); // Send data to client
