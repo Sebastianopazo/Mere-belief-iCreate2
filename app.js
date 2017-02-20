def4cb1e160a61d0de2f4c7d7a39450090ebf1c7
macy260admin <macy260admin@naokita-2.local>
2017-02-19 19:15:51 -0500

    added audio file

--- app.js
+++ app.js
@@ -159,27 +159,3 @@
 //
 //
 // start();
-
-
-var player = require('play-sound')(opts = {})
-
-// $ mplayer foo.mp3
-player.play('monkeys.mp3', function(err){
-  if (err) throw err
-})
-
-// { timeout: 300 } will be passed to child process
-player.play('monkeys.mp3', { timeout: 300 }, function(err){
-  if (err) throw err
-})
-
-// configure arguments for executable if any
-player.play('monkeys.mp3', { afplay: ['-v', 1 ] /* lower volume for afplay on OSX */ }, function(err){
-  if (err) throw err
-})
-
-// access the node child_process in case you need to kill it on demand
-var audio = player.play('monkeys.mp3', function(err){
-  if (err && !err.killed) throw err
-})
-audio.kill();
