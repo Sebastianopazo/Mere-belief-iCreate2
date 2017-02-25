4a08e17f46150d1c60644889f88c5b927ef2cad9
macy260admin <macy260admin@naokita-2.local>
2017-02-25 02:15:20 -0500

    fixed dependencies

--- app.js
+++ app.js
@@ -176,6 +176,26 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
-    var led = new five.Led(15);
-    led.blink();
+
+  var matrix = new five.Led.Matrix({
+    pins: {
+      data: 2,
+      clock: 3,
+      cs: 4
+    },
+    devices: 1
+  });
+
+  var heart = [
+    "01100110",
+    "10011001",
+    "10000001",
+    "10000001",
+    "01000010",
+    "00100100",
+    "00011000",
+    "00000000"
+  ];
+
+  matrix.draw(heart);
 });
