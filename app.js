a4a3f6f71b0f35d9a6cbe74bb077ba79f57135a9
macy260admin <macy260admin@naokita-2.local>
2017-02-25 01:14:45 -0500

    fixed dependencies

--- app.js
+++ app.js
@@ -178,3 +178,4 @@ board.on('ready', () => {
   // Create an Led on pin 7 (GPIO4) on P1 and strobe it on/off
   // Optionally set the speed; defaults to 100ms
   (new five.Led('P1-7')).strobe();
+});
