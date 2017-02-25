abce20f2404b7884403777f3a13de7f38ccbef6b
macy260admin <macy260admin@naokita-2.local>
2017-02-25 01:15:59 -0500

    fixed dependencies

--- app.js
+++ app.js
@@ -167,15 +167,34 @@
 // })
 
 
-const Raspi = require('raspi-io');
-const five = require('johnny-five');
-const board = new five.Board({
-  io: new Raspi()
-});
+var five = require("johnny-five");
+var board = new five.Board();
 
-board.on('ready', () => {
+board.on("ready", function() {
 
-  // Create an Led on pin 7 (GPIO4) on P1 and strobe it on/off
-  // Optionally set the speed; defaults to 100ms
-  (new five.Led('P1-7')).strobe();
+  var matrix = new five.Led.Matrix({
+    pins: {
+      data: 2,
+      clock: 3,
+      cs: 4
+    }
+  });
+
+  matrix.on();
+
+  /*
+
+  A single Led.Matrix object can control one
+  or more led matrix devices. All methods of
+  Led.Matrix objects expect the target device's
+  index as their first argument. Since this
+  might seem cumbersome when there is only
+  one matrix device, use the `device()` method
+  to create a display device bound to an index.
+
+  */
+
+  this.repl.inject({
+    display: matrix.device(0)
+  });
 });
