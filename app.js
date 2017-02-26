ca39627930e57e46b99f9ce75e0ce52d304b844a
macy260admin <macy260admin@naokita-2.local>
2017-02-25 21:08:17 -0500

    fixed dependencies

--- app.js
+++ app.js
@@ -171,6 +171,7 @@ var LedMatrix = require("node-rpi-rgb-led-matrix");
 
 var matrix = new LedMatrix(8);
 var caca = matrix.getHeight();
-console.log(caca);
+var poto = matrix.getWidth();
+console.log(caca, poto);
 matrix.fill(255, 50, 100);
 matrix.setPixel(0, 0, 0, 50, 255);
