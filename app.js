12e153fc4d40937a454dad0df52f837be7951201
macy260admin <macy260admin@naokita-2.local>
2017-02-26 02:25:33 -0500

    restored old functions

--- app.js
+++ app.js
@@ -169,6 +169,6 @@
 
 var LedMatrix = require("node-rpi-rgb-led-matrix");
 
-var matrix = new LedMatrix(16);
+var matrix = new LedMatrix(8);
 matrix.fill(255, 50, 100);
 matrix.setPixel(0, 0, 0, 50, 255);
