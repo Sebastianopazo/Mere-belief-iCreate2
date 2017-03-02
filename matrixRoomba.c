2f11057d78b3844b494619997e7466bcc2ef7d23
macy260admin <macy260admin@naokita-2.local>
2017-03-02 04:07:47 -0500

    working on led matrix

--- matrixRoomba.c
+++ matrixRoomba.c
@@ -14,22 +14,22 @@ int main(void)
 
   wiringPiSetup();
   wiringPiSPISetup(0,500000);
-
-void yellowscreen()
-   {
-    int j;
-    int x = 1;
-    static uint8_t heart[8] = {0x00, 0x66, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18};
-    for ( j=0;j<8;j++)
-                {
-          data[0] = 0x00;
-          data[2] = 0xFF;
-          data[1] = 0x00;
-          data[3] = 0xFF;
-          wiringPiSPIDataRW(0,data,sizeof(data));
-          delay(x);
-                }
-};
+// 
+// void yellowscreen()
+//    {
+//     int j;
+//     int x = 1;
+//     static uint8_t heart[8] = {0x00, 0x66, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18};
+//     for ( j=0;j<8;j++)
+//                 {
+//           data[0] = 0x00;
+//           data[2] = 0xFF;
+//           data[1] = 0x00;
+//           data[3] = 0xFF;
+//           wiringPiSPIDataRW(0,data,sizeof(data));
+//           delay(x);
+//                 }
+// };
 
 void whitescreen()
    {
