243a2a881bfd0a07397c9475a5e1b6656a70b1b5
macy260admin <macy260admin@naokita-2.local>
2017-03-02 01:00:00 -0500

    added gesture randomizer

--- app.js
+++ app.js
@@ -167,7 +167,8 @@ function main(r) {
     for (var i = 0; i < gestureQuantity; i++) {
         var addedTime = gestureDuration*i;
         setTimeout(function() {
-        stop;
+        // stop;
+        console.log('stopping')
           setTimeout(function() {
             // var randomGesture = gesture[r.get()]();
             // randomGesture;
