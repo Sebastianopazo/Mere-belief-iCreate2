466d644599d564a2e447a79b47deda5abc64e93e
macy260admin <macy260admin@naokita-2.local>
2017-03-02 04:15:45 -0500

    working on led matrix

--- matrixRoomba.c
+++ matrixRoomba.c
@@ -19,13 +19,13 @@ void heartbig()
    {
     int j;
     int x = 2;
-    static uint8_t heart[8] = {0x00, 0x66, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18};
+    static uint8_t heart[8] = {0x7E};
     for ( j=0;j<8;j++)
                 {
-          // data[0] = ~heart[j];
-          // data[2] = 0xFF;
-          // data[1] = 0xFF;
-          // data[3] = 0x01 << j ;
+          data[0] = ~heart[j];
+          data[2] = 0xFF;
+          data[1] = 0xFF;
+          data[3] = 0x01 << j ;
           wiringPiSPIDataRW(0,data,sizeof(data));
           delay(x);
                 }
