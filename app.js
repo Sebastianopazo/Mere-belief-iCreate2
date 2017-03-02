18d3aab90b19363669be68555aa0d8fad46da28d
macy260admin <macy260admin@naokita-2.local>
2017-03-02 02:37:37 -0500

    added gesture randomizer

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const player = require('play-sound')(opts = {});
 //ROBOT Communication and Behaviors
 
 var create = require('create2');
-var start,robot, turnRobot, stopTurn, moveForward, stop, moveBackward, turnRight, turnLeft, answer1Server, answer2, answer3, answer4, answer5, answer6, answer7, amswer8, answer9;
+var start, stopAll, robot, turnRobot, stopTurn, moveForward, stop, moveBackward, turnRight, turnLeft, answer1Server, answer2, answer3, answer4, answer5, answer6, answer7, amswer8, answer9;
 
 start = function () {
 	create.prompt(function(p){create.open(p,main)});
@@ -207,15 +207,18 @@ stop = function() {
 
 answer1Server = function() {
   player.play('audio/answer1.mp3', function(err){
-  if (err) throw err
-  })
-  var audio = player.play('audio/answer1.mp3', function(err){
-    if (err && !err.killed) throw err
-  })
+      if (err) throw err
+    })
+
     behaviorRandomizer(22, 35);
   };
+
   stopAll = function() {
+    var audio = player.play('audio/answer1.mp3', function(err){
+      if (err && !err.killed) throw err
+    })
     audio.kill();
+    stop();
   };
 
 }
