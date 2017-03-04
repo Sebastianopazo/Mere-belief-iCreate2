1ec93f6985874a44422c93d370fed1ab9cb0af48
macy260admin <macy260admin@naokita-2.local>
2017-03-04 18:24:18 -0500

    working on led matrix

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ function main(r) {
       robot.setSong(0, [[71,12],[77,12],[77,12],[77,36],[76,36],[74,36],[72,24],[67,12],[64,48], [60,48]]);
       setTimeout(function(){
       robot.stop();
-    }, 7000);
+    }, 1000);
     };
 
   stop = function() {
