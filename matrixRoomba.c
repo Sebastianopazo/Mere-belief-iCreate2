8ec66dbe06809f6d9ab3053da290abcce87b0e5e
macy260admin <macy260admin@naokita-2.local>
2017-03-04 22:24:06 -0500

    working on led matrix

--- matrixRoomba.c
+++ matrixRoomba.c
@@ -13,13 +13,13 @@ int main(void)
   static uint8_t data[4] = {0x0,0x0,0x0,0x0};                  // initialize RGB data source
   static uint8_t i = 0;
 
-  wiringPiSetup();                                // initialize wiringPi
-  wiringPiSPISetup(0,500000);           // initialize SPI  information, 0 is channel 0, 500000 is clock rate.
+  wiringPiSetup();                 // initialize wiringPi
+  wiringPiSPISetup(0,500000);      // initialize SPI  information, 0 is channel 0, 500000 is clock rate.
   while(1){
     int j;
     int x = 2;
     static uint8_t heart[8] = {0x00, 0x66, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18};             // this is a array of heart
-    for ( j=0;j<8;j++)
+    for ( j=7;j<8;j++)
 {
           data[0] = ~heart[j];
           data[2] = 0xFF;
