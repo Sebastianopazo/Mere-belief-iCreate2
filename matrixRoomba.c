ff57b3ad84236e64dc6fba342b7997c5c1fc93d4
macy260admin <macy260admin@naokita-2.local>
2017-03-04 22:22:17 -0500

    working on led matrix

--- matrixRoomba.c
+++ matrixRoomba.c
@@ -1,81 +1,32 @@
+
 #include <stdio.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 #include <stdint.h>
 
-#define RED_DATA 0
-#define BLUE_DATA 1
-#define GREEN_DATA 2
+#define RED_DATA 0          //define the red data source
+#define BLUE_DATA 1         //define the blue data source
+#define GREEN_DATA 2    // define the green data source
 
 int main(void)
 {
-  static uint8_t data[4] = {0x0,0x0,0x0,0x0};
+  static uint8_t data[4] = {0x0,0x0,0x0,0x0};                  // initialize RGB data source
   static uint8_t i = 0;
 
-  wiringPiSetup();
-  wiringPiSPISetup(0,500000);
-
-void heartbig()
-   {
+  wiringPiSetup();                                // initialize wiringPi
+  wiringPiSPISetup(0,500000);           // initialize SPI  information, 0 is channel 0, 500000 is clock rate.
+  while(1){
     int j;
     int x = 2;
-    static uint8_t heart[8] = {0x3E, 0x63, 0x73, 0x7B, 0x6F, 0x67, 0x3E, 0x00};
+    static uint8_t heart[8] = {0x00, 0x66, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18};             // this is a array of heart
     for ( j=0;j<8;j++)
-                {
-          data[0] = ~heart[j];
-          data[2] = ~heart[j];
-          data[1] = ~heart[j];
-          data[3] = 0x01 << j ;
-          wiringPiSPIDataRW(0,data,sizeof(data));
-          delay(x);
-                }
-};
-
-void heartsmall()
-   {
-    int j;
-    int x = 2;
-    static uint8_t heart[8] = {0x00, 0x00, 0x24, 0x7E, 0x7E, 0x3C, 0x18, 0x00};
-    for ( j=0;j<8;j++)
-                {
-          data[0] = ~heart[j];
-          data[2] = 0xFF;
-          data[1] = 0xFF;
-          data[3] = 0x01 << j ;
-          wiringPiSPIDataRW(0,data,sizeof(data));
-          delay(x);
-                }
-};
-
-void matrixoff()
 {
- int j;
- int x = 2;
-    static uint8_t heart[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    for ( j=0;j<8;j++)
-     {
           data[0] = ~heart[j];
           data[2] = 0xFF;
           data[1] = 0xFF;
           data[3] = 0x01 << j ;
-          wiringPiSPIDataRW(0,data,sizeof(data));
+          wiringPiSPIDataRW(0,data,sizeof(data));              // send data to SPI channel 0, and the length of the data
           delay(x);
-       }
-}
-
-while(1){
-     int m = 10;
-     for ( m=10; m>0; m--)
-        {
-         heartbig();
-         };
-         matrixoff();
-        delay(100);
-     for ( m=10; m>0; m--)
-         {
-         heartsmall();
-         };
-         matrixoff();
-         delay(100);
-    }
+   }
+  }
 }
