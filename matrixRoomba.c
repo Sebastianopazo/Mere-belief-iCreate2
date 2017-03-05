f55239a18aa08acd86f49fa80f0e384d9455bc56
macy260admin <macy260admin@naokita-2.local>
2017-03-04 23:23:35 -0500

    working on led matrix

--- matrixRoomba.c
+++ matrixRoomba.c
@@ -15,18 +15,92 @@ int main(void)
 
   wiringPiSetup();                                // initialize wiringPi
   wiringPiSPISetup(0,500000);           // initialize SPI  information, 0 is channel 0, 500000 is clock rate.
-  while(1){
+
+
+  void out()
+   {
     int j;
     int x = 2;
-    static uint8_t pixel[8] = {0x80,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+    static uint8_t heart[8] = {0xff,0x81,0x81,0x81,0x81,0x81,0x81,0xff};
     for ( j=0;j<8;j++)
-{
-          data[0] = ~pixel[j];
-          data[1] = ~pixel[j];
-          data[2] = ~pixel[j];
+                {
+          data[0] = ~heart[j];
+          data[2] = ~heart[j];
+          data[1] = ~heart[j];
           data[3] = 0x01 << j ;
-          wiringPiSPIDataRW(0,data,sizeof(data));              // send data to SPI channel 0, and the length of the data
+          wiringPiSPIDataRW(0,data,sizeof(data));
           delay(x);
-   }
-  }
+                }
+};
+
+    void in()
+     {
+      int j;
+      int x = 2;
+      static uint8_t heart[8] = {0x00,0x00,0x00,0x18,0x18,0x00,0x00,0x00};
+      for ( j=0;j<8;j++)
+                  {
+            data[0] = ~heart[j];
+            data[2] = ~heart[j];
+            data[1] = ~heart[j];
+            data[3] = 0x01 << j ;
+            wiringPiSPIDataRW(0,data,sizeof(data));
+            delay(x);
+                  }
+    };
+
+    void mid()
+     {
+      int j;
+      int x = 2;
+      static uint8_t heart[8] = {0x00,0x7e,0x7e,0x66,0x66,0x7e,0x7e,0x00};
+      for ( j=0;j<8;j++)
+                  {
+            data[0] = ~heart[j];
+            data[2] = ~heart[j];
+            data[1] = ~heart[j];
+            data[3] = 0x01 << j ;
+            wiringPiSPIDataRW(0,data,sizeof(data));
+            delay(x);
+                  }
+    };
+
+    void matrixoff()
+    {
+     int j;
+     int x = 2;
+        static uint8_t heart[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+        for ( j=0;j<8;j++)
+         {
+              data[0] = ~heart[j];
+              data[2] = 0xFF;
+              data[1] = 0xFF;
+              data[3] = 0x01 << j ;
+              wiringPiSPIDataRW(0,data,sizeof(data));
+              delay(x);
+           }
+    }
+
+    while(1){
+         int m = 10;
+         for ( m=10; m>0; m--)
+            {
+             out();
+             };
+             matrixoff();
+            delay(100);
+         for ( m=10; m>0; m--)
+             {
+             in();
+             };
+             matrixoff();
+            delay(100);
+         for ( m=10; m>0; m--)
+             {
+              mid();
+             };
+        }
+    }
+
+
 }
