ddc29517e429615fb8c515d9540c83c891ccfe55
macy260admin <macy260admin@naokita-2.local>
2017-03-07 13:33:36 -0500

    added mp3 song

--- matrixRoomba.c
+++ matrixRoomba.c
@@ -21,12 +21,12 @@ int main(void)
    {
     int j;
     int x = 2;
-    static uint8_t heart[8] = {0xff,0x81,0x81,0x81,0x81,0x81,0x81,0xff};
+    static uint8_t out[8] = {0xff,0x81,0x81,0x81,0x81,0x81,0x81,0xff};
     for ( j=0;j<8;j++)
                 {
           data[0] = 0xFF;
-          data[2] = ~heart[j];
-          data[1] = ~heart[j];
+          data[2] = 0xFF;
+          data[1] = 0xFF;
           data[3] = 0x01 >> j ;
           wiringPiSPIDataRW(0,data,sizeof(data));
           delay(x);
@@ -37,10 +37,10 @@ int main(void)
      {
       int j;
       int x = 2;
-      static uint8_t heart[8] = {0x00,0x00,0x3c,0x3c,0x3c,0x3c,0x00,0x00};
+      static uint8_t in[8] = {0x00,0x00,0x3c,0x3c,0x3c,0x3c,0x00,0x00};
       for ( j=0;j<8;j++)
                   {
-            data[0] = ~heart[j];
+            data[0] = ~in[j];
             data[2] = 0xFF;
             data[1] = 0xFF;
             data[3] = 0x01 << j ;
