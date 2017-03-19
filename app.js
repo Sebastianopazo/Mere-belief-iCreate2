e40d506b6533701ad2af128a8f4aacc0be2bdfa5
Sebastianopazo <so2453@tc.columbia.edu>
2017-03-19 19:37:11 -0400

    python shell commands

--- app.js
+++ app.js
@@ -249,13 +249,11 @@ function main(r) {
     behaviorRandomizer(duration, gestureQuantity);
     var options = {
       mode: 'text',
-      pythonPath: '/lightshowpi/py/',
       pythonOptions: ['file=/var/www/html/Roomba/audio/answer'+ answerNum +'.mp3'],
-      scriptPath: '/lightshowpi/py/',
       //args: ['file=/var/www/html/Roomba/audio/answer'+ answerNum +'.mp3']
     };
 
-    PythonShell.run('synchronized_lights.py', options, function (err, results) {
+    PythonShell.run('/lightshowpi/py/synchronized_lights.py', options, function (err, results) {
       if (err) throw err;
       // results is an array consisting of messages collected during execution
       console.log('results: %j', results);
