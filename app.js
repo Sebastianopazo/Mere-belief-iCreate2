d5a946901649508580da8c2be02588a3e4804829
Sebastianopazo <so2453@tc.columbia.edu>
2017-03-19 20:51:03 -0400

    python shell commands

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-// var child_process = require('child_process');
+var child_process = require('child_process');
 var PythonShell = require('python-shell');
 
 app.use(express.static(__dirname + '/node_modules'));
@@ -264,7 +264,7 @@ function main(r) {
       clearTimeout(timeouts[i]);
     };
     stop();
-    PythonShell.kill('SIGINT');
+    PythonShell.child_process.kill('SIGINT');
   };
 
 }
