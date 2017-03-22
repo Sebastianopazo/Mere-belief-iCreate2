af91da26b6d959bb1624c3a8842b14c08700b3e4
Sebastianopazo <so2453@tc.columbia.edu>
2017-03-22 17:48:08 -0400

    added text to speech functions

--- app.js
+++ app.js
@@ -420,12 +420,5 @@ var say = require('say');
 // Fire a callback once the text has completed being spoken
 
 function speakServer() {
-  // Fire a callback once the text has completed being spoken
-say.speak('whats up, dog?', function(err) {
-  if (err) {
-    return console.error(err);
-  }
-
-  console.log('Text has been spoken.');
-});
+  say.speak('Hello!');
 }
