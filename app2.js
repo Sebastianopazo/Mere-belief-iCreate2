9157db2b9df9f1b9eba63506a61eb3f627212f21
Sebastianopazo <so2453@tc.columbia.edu>
2018-01-03 22:04:52 -0500

    testing

--- app2.js
+++ app2.js
@@ -17,58 +17,56 @@ Polly.Speak('I like potatoes and other vegetables, such as tomatoes or penises')
 //	var pulseWidth = Math.round(500 + (servoDegrees*(2000/180)));
 	//Variable to identify servo based on GPIO pin number
 //	var motor = new Gpio(servoPin, {mode: Gpio.OUTPUT});
-	
+
 //	if (pulseWidth>499&&pulseWidth<2501){
 //		var interval = setInterval(function () {
-//			motor.servoWrite(pulseWidth);	
-//		}, 10); 
+//			motor.servoWrite(pulseWidth);
+//		}, 10);
 //	}else{
 //		console.log("You can only use values between 0 and 180");
 //	}
-//} 
+//}
 
 //moveServo(17, 180);
-
-//wpi.setup('wpi');
-//wpi.wiringPiSPISetup(0, 500000);
-//const RED_DATA = 0;
-//const BLUE_DATA = 1;
-//const GREEN_DATA = 2;
-
-//const data = Buffer.from([0x0,0x0,0x0,0x0]);
-//const heart = Buffer.from([0xff,0x99,0x00,0x00,0x00,0x81,0xc3,0xe7]);
-
 //var motor = new Gpio(17, {mode: Gpio.OUTPUT});
 //var motor2 = new Gpio(21, {mode: Gpio.OUTPUT});
 
+wpi.setup('wpi');
+wpi.wiringPiSPISetup(0, 500000);
+const RED_DATA = 0;
+const BLUE_DATA = 1;
+const GREEN_DATA = 2;
 
-//setInterval(function() {
-//	while(true){
-//		for(j=0;j<8;j++){
-//			data[0] = 0xFF;
-//			data[2] = heart[j];
-//			data[1] = 0xFF;
-//			data[3]	= 0x01 << j;	
-//			wpi.wiringPiSPIDataRW(0,data);	
-//		}
-//
-//	var pulseWidth = 1000;
-//	var increment = 100;
-//
-//	//setInterval(function(){
-//		motor.servoWrite(pulseWidth);
-//		motor2.servoWrite(pulseWidth);
-//		pulseWidth += increment;
-//			
-//		if(pulseWidth>=2500){
-//			increment = -100;
-//		} else if (pulseWidth<=500){
-//			increment = 100;
-//		}
-//		
-//
-//	//}, 30)
-//	}
-//	
-//
-//}, 0.00001);
+const data = Buffer.from([0x0,0x0,0x0,0x0]);
+const heart = Buffer.from([0xff,0x99,0x00,0x00,0x00,0x81,0xc3,0xe7]);
+
+setInterval(function() {
+	//while(true){
+		for(j=0;j<8;j++){
+			data[0] = 0xFF;
+			data[2] = heart[j];
+			data[1] = 0xFF;
+			data[3]	= 0x01 << j;
+			wpi.wiringPiSPIDataRW(0,data);
+		//}
+
+	// var pulseWidth = 1000;
+	// var increment = 100;
+  //
+	// //setInterval(function(){
+	// 	motor.servoWrite(pulseWidth);
+	// 	motor2.servoWrite(pulseWidth);
+	// 	pulseWidth += increment;
+  //
+	// 	if(pulseWidth>=2500){
+	// 		increment = -100;
+	// 	} else if (pulseWidth<=500){
+	// 		increment = 100;
+	// 	}
+  //
+  //
+	// //}, 30)
+	}
+
+
+}, 0.00001);
